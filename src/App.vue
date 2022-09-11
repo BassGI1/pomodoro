c65a21a420383da2ac3d7181ab94e021eee7dc00
Bassam <basmaym148@gmail.com>
2022-09-11 14:09:14 -0400

    timer done (at least for now)

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        timer: 5,
+        interval: false,
+        timeout: false,
+        stopped: true
+      }
+    },
+    methods: {
+      count: function() {
+        this.interval = setInterval(() => {
+          --this.timer
+        }, 1000)
+        this.stopped = !this.stopped
+        if (this.interval){
+          this.timeout = setTimeout(() => clearInterval(this.interval), this.timer*1000)
+        }
+      },
+      clear: function() {
+        clearTimeout(this.timeout)
+        clearInterval(this.interval)
+        this.stopped = !this.stopped
+      }
+    },
+    mounted() {
+      this.count()
+    }
+  }
+</script>
+
+
+
+<template>
+  <div>
+    {{timer}}
+  </div>
+  <button v-if="!stopped" @click="clear">
+    Stop
+  </button>
+  <button v-if="stopped" @click="count">
+    Start
+  </button>
+</template>
+
+
+
+<style>
+
+</style>
